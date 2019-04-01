455a31c0a25990806e374a9e3ce0d3adfbb40964
tjdi226 <47254474+tjdi226@users.noreply.github.com>
2019-04-01 15:29:14 -0400

    q2
    q2

--- jenkins/core/src/main/java/jenkins/model/Nodes.java
+++ jenkins/core/src/main/java/jenkins/model/Nodes.java
@@ -94,6 +94,12 @@ public class Nodes implements Saveable {
     public List<Node> getNodes() {
         return new ArrayList<Node>(nodes.values());
     }
+    
+    public void updateAndTrim()
+    {
+        jenkins.updateComputerList();
+        jenkins.trimLabels();
+    }
 
     /**
      * Sets the list of nodes.
@@ -112,8 +118,9 @@ public class Nodes implements Saveable {
                     Nodes.this.nodes.put(name, n);
                 }
                 Nodes.this.nodes.keySet().removeAll(toRemove); // directory clean up will be handled by save
-                jenkins.updateComputerList();
-                jenkins.trimLabels();
+                //jenkins.updateComputerList();
+                //jenkins.trimLabels();
+                updateAndTrim();
             }
         });
         save();
@@ -133,8 +140,9 @@ public class Nodes implements Saveable {
                 @Override
                 public void run() {
                     nodes.put(node.getNodeName(), node);
-                    jenkins.updateComputerList();
-                    jenkins.trimLabels();
+                    //jenkins.updateComputerList();
+                    //jenkins.trimLabels();
+                    updateAndTrim();
                 }
             });
             // no need for a full save() so we just do the minimum
@@ -166,8 +174,9 @@ public class Nodes implements Saveable {
                         c.disconnect(OfflineCause.create(hudson.model.Messages._Hudson_NodeBeingRemoved()));
                     }
                     if (node == nodes.remove(node.getNodeName())) {
-                        jenkins.updateComputerList();
-                        jenkins.trimLabels();
+                        //jenkins.updateComputerList();
+                        //jenkins.trimLabels();
+                        updateAndTrim();
                     }
                 }
             });
@@ -251,8 +260,9 @@ public class Nodes implements Saveable {
                     }
                 }
                 nodes.putAll(newNodes);
-                jenkins.updateComputerList();
-                jenkins.trimLabels();
+                //jenkins.updateComputerList();
+                //jenkins.trimLabels();
+                updateAndTrim();
             }
         });
     }
